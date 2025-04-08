200025197d7ab71215cc8d3a765489efbed79968
Javier Roman <javier.rmgz@gmail.com>
2025-04-08 11:43:34 +0200

    Remove old integration tests

--- server/src/test/resources/updateContract.sh
+++
@@ -1,2 +0,0 @@
-solc EventEmitter.sol --overwrite --bin --abi --optimize -o compiled/
-web3j solidity generate -b compiled/EventEmitter.bin -a compiled/EventEmitter.abi -o ../java -p io.librevents.server
